3a8c01fae4fd66e53b600d3425e1a4cf5621cffc
Tye <tyewalker09@gmail.com>
2021-05-11 20:17:14 +0200

    Created Supplier class Added Getters, Setters, Constructor

--- src/main/java/za/ac/cput/Supplier.java
+++ src/main/java/za/ac/cput/Supplier.java
@@ -10,5 +10,46 @@ package za.ac.cput;
 
 public class Supplier {
 
+    private String supName, supContact;
+    private double accBalance;
+
+    public Supplier(String supName, String supContact, double accBalance) {
+        this.supName = supName;
+        this.supContact = supContact;
+        this.accBalance = accBalance;
+    }
+
+    public String getSupName() {
+        return supName;
+    }
+
+    public void setSupName(String supName) {
+        this.supName = supName;
+    }
+
+    public String getSupContact() {
+        return supContact;
+    }
+
+    public void setSupContact(String supContact) {
+        this.supContact = supContact;
+    }
+
+    public double getAccBalance() {
+        return accBalance;
+    }
+
+    public void setAccBalance(double accBalance) {
+        this.accBalance = accBalance;
+    }
+
+    @Override
+    public String toString() {
+        return "Supplier{" +
+                "supName='" + supName + '\'' +
+                ", supContact='" + supContact + '\'' +
+                ", accBalance=" + accBalance +
+                '}';
+    }
 
 }
