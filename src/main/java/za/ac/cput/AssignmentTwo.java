0df51873a648c9a96f7ef34b138ebf18d3ca7894
Tye <tyewalker09@gmail.com>
2021-05-11 20:15:21 +0200

    Updated iv. List Added: Add, Remove, Find

--- src/main/java/za/ac/cput/AssignmentTwo.java
+++ src/main/java/za/ac/cput/AssignmentTwo.java
@@ -15,10 +15,7 @@ package za.ac.cput;
  * iv.  List
  */
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class AssignmentTwo {
 
@@ -33,6 +30,11 @@ public class AssignmentTwo {
     Staff staffOne = new Staff(1234, "John", "Doe", "1990-05-29", "Maintenance", 25.00);
     Staff staffTwo = new Staff(1314, "Jane", "Flower", "1996-12-29", "Accountant", 40.59);
     Staff staffThree = new Staff(1234, "Bruce", "Wayne", "1979-06-06", "CEO", 1);
+
+    //Item Objects:
+    Item itemOne = new Item("Phone", 6999.95);
+    Item itemTwo = new Item("Jacket", 5499.95);
+    Item itemThree = new Item("CD", 50.00);
 
     //i.    Collections:
 
@@ -91,11 +93,35 @@ public class AssignmentTwo {
     public void findStaff() {
         //Adding staff:
         addStaff();
-        //Finding staff: Returns true if staff found
+        //Finding staff: Prints true if staff found
         System.out.println(staffMembers.contains(staffThree));
     }
 
     //iv.   List
+
+    List<Item> listItem = new ArrayList<>();
+
+    //Add:
+    public void addItem() {
+        listItem.add(itemOne);
+        listItem.add(itemTwo);
+        listItem.add(itemThree);
+    }
+
+    //Remove:
+    public void removeItemObj() {
+        //Adding objects:
+        addItem();
+        //Remove itemTwo:
+        listItem.remove(itemTwo);
+    }
+
+    public void findItem() {
+        //Adding objects:
+        addItem();
+        //Finding item: Prints true if list contains itemThree
+        System.out.println(listItem.contains(itemThree));
+    }
 
 
     public static void main(String[] args) {
