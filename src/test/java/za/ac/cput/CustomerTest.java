9c72193e516678fd38bbcb4ae9ca248a211eefc6
Tye <tyewalker09@gmail.com>
2021-05-12 18:44:26 +0200

    Added comments

--- src/test/java/za/ac/cput/CustomerTest.java
+++ src/test/java/za/ac/cput/CustomerTest.java
@@ -1,5 +1,14 @@
 package za.ac.cput;
 
+/*
+ * @Author: Tye Walker
+ * Student Number: 218338562
+ *
+ * Map Test using Customer class
+ * Test for:    Add, Remove, Find
+ *
+ */
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
